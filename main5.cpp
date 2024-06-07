626ab5963c4f153c3c563c068596f33ce99427f3
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-07 21:26:50 +0530

    Add files via upload

---
+++ main5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+//for loop
+//counting fron 1 to 5
+ 
+// int main(){
+//     for(int i = 1; i<=10;i=i+1){
+//         cout<<i<<endl;
+//     }
+// }
+
+//break in loops
+// int main(){
+//     for(int i = 1; i<=10;i=i+1){
+//         cout<<i<<"";
+//         if (i==5){
+//             break;
+//         }
+//     }
+// }
+
+//continue in loop
+// int main(){
+//     for(int i = 1; i<=5;i=i+1){
+        
+//         if (i==3){
+//             continue;;
+//         }
+//         cout<<i<<"";
+//     }
+//     return 0;
+
+// }
+
+//while loop
+int main(){
+    int i= 1;
+    while(i<=5){
+        cout<< i <<"";
+        i = i+1;
+    }
+    return 0;
+}
+    
